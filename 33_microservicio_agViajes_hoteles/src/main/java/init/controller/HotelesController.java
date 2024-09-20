616c931462b8cf1e1646535ad8e48d32228d7924
ANA <Usuario@host.docker.internal>
2024-09-20 17:59:43 +0200

    funciona

--- 33_microservicio_agViajes_hoteles/src/main/java/init/controller/HotelesController.java
+++ 33_microservicio_agViajes_hoteles/src/main/java/init/controller/HotelesController.java
@@ -1,5 +1,7 @@
 package init.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,9 +11,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 import init.Service.HotelesService;
 import init.dto.HotelDto;
+
 
 
 
@@ -40,6 +42,17 @@ public class HotelesController {
 		
 	}
 	
+	@GetMapping(value="buscarLocalizacion/{localizacion}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<HotelDto>> buscarPorLocalizacion(@PathVariable("localizacion") String localizacion){
+		List<HotelDto> hoteles = hotelesService.buscarPorLocalizacion(localizacion);
+		
+		if(hoteles!=null && !hoteles.isEmpty()) {
+			
+			return new ResponseEntity<>(hoteles, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+		
 	
 
 }
