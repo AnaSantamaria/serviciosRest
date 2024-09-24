1d51aa1ce42234851717aca87908c94be9624fad
manana <you@example.com">
2024-09-24 12:56:04 +0200

    antes de postman

--- 35_microservicio_agViajes_reservas/src/main/java/init/controller/ReservasController.java
+++ 35_microservicio_agViajes_reservas/src/main/java/init/controller/ReservasController.java
@@ -1,7 +1,59 @@
 package init.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import init.model.ReservasDto;
+import init.service.ReservasService;
+
+// Alta de la misma con los datos de reserva (securizado para que solo usuarios de un 
+//determinado rol puedan utilizarlo). //Lista de reservas por cliente
+
+
+@CrossOrigin("*")
+@RestController
+
 public class ReservasController {
 	
+	@Autowired
+	ReservasService reservasService;
 	
-
+	@PostMapping(value = "alta/{plazas}", consumes=MediaType.APPLICATION_JSON_VALUE) 
+public ResponseEntity<String> altaReserva (@RequestBody ReservasDto reservaDto, @RequestParam int plazas){
+	
+	boolean nuevoAlta = reservasService.altaReserva(reservaDto, plazas);
+	
+	if (nuevoAlta != false) {
+		
+		 return ResponseEntity.ok("reserva creada correctamente");
+		
+		
+	}
+	
+	return ResponseEntity.badRequest().body("Error al crear la reserva.");
+	
+}
+	@GetMapping (value= "reservasPorCliente/{usuario}")
+	public ResponseEntity<List<ReservasDto>> reservasPorCliente (@PathVariable String usuario){
+		
+		List<ReservasDto> listaReservas =reservasService.listaDeReservas(usuario);
+		
+		if(listaReservas !=  null) {
+			
+			return new ResponseEntity<>(listaReservas, HttpStatus.OK);
+		}
+		
+		 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
 }
